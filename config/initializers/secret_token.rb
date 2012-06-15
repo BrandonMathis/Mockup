d6c595e47780ed93518a91fb15aba9cfcfa3ac60
BrandonMathis <Brandon@KeysetTS.com>
2012-06-15 11:50:35 -0400

    checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mockup::Application.config.secret_token = '34c312a0df96bd1f3fce34da57cee6bb0b5d8cde8b0d109b1aeeab3b8ab3da6ac18fbe25177f1f1e439c60b0464530af0660d7d8105d945664a0de18c6a13eae'
