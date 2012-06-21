4068498b5abc4cd4bec35c758e7a489506c463da
BrandonMathis <Brandon@KeysetTS.com>
2012-06-21 18:14:30 -0400

    Added a front page and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Mockup::Application.routes.draw do
-  root :to => 'high_voltage/pages#show', :id => 'about'
+  root :to => 'high_voltage/pages#show', :id => 'front'
+  match '/page', to: 'high_voltage/pages#show', id: 'about'
 end
